b6f7fb7a1a6019c907a90c727478b62802e338a4
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-16 16:25:22 +0500

    Update Comman.jsx

--- src/Comman.jsx
+++ src/Comman.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import web from "../src/images/images.jpeg";
 import { NavLink } from "react-bootstrap";
-const Comman=()=>{
+const Comman=(props)=>{
     return(
    
       <>
@@ -14,19 +14,19 @@ const Comman=()=>{
           <div className="col-10 mx_auto">
           <div className="row">
           <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-            <h1>Grow Your Business with <strong className="brand-name">{} </strong></h1>
+            <h1>Grow Your Business with <strong className="brand-name">{props.name} </strong></h1>
             <h2 className="my-3">
             We are the team of talented developer making websites
             </h2>
             <div className="mt-3">
 
 
-          <NavLink to="/service" className="btn-get-started">Get Started</NavLink>
+          <NavLink to={props.visit} className="btn-get-started">{props.btname}</NavLink>
 
             </div>
           </div>
           <div className="col-lg-6 order-1 order-lg-2 header-img">
-            <img src={web} className="img-fluid animated" alt="home img"/>
+            <img src={props.imgsrc} className="img-fluid animated" alt="home img"/>
           </div>
           </div>
           </div>
