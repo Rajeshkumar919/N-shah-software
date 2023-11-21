66b4b6bc94a5bd4cfae79be071e34117e9901a4b
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-21 10:21:56 +0500

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,75 @@
 
 import React from "react";
 const Contact=()=>{
+
+ const [data,setData]=useState({
+
+fulname : "",
+     phone: "",
+     msg: "",
+     
+ });
+    const InputEvent=(event)=>{
+
+const {name,value}=event.target;
+        setSdata((preVal)={
+            ...preVal,
+            [name]: value,
+            
+        };
+        
+    });
+}
+    const formSubmit = () =>{
+
+e.preventdefault()
+        alert(`My name is ${data.fullname}. My mobile number is ${data.phone}, Here is what i want to say ${data.msg}`);
+        
+    };
+    
+    
     return(
    
       <>
-      <h1>Welcom Contact  Page</h1>
+      <div className="my-5">
+          <h1 className="text-center">Contact Us</h1>
+      </div>
+          <div className="container contact_div">
+          <div className="row">
+          
+          <div className="col-md-6 col-10 mx-auto">
+          <form onSubmit={formSubmit}>
+          <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1"
+      name="fullname"
+      value={data.fullname}
+          onChange={InputEvent}
+      
+      
+      placeholder="Enter Your Name">
+</div>
+              <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">phone</label>
+  <input type="number" class="form-control" id="exampleFormControlInput1" 
+       name="phone"
+      value={data.phone}
+          onChange={InputEvent}
+      placeholder="mobile number">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+          <div className="col-12">
+              
+              <button className="btn btn-outline-primary" type="submit">Submit form</Button>
+              </div>
+          </form>
+          </div>
+          </div>
+          
+          </div>
       </>
         
     );
