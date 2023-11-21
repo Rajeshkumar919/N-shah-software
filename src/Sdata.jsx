e9e893fa07f873b856fc595f68fba86936bd60c1
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-21 09:41:31 +0500

    Update Sdata.jsx

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,1 +1,40 @@
+import web from "../src/images/img1.jpg";
+import app from "../src/images/app.jpg";
+import android from "../src/images/android.jpg";
+import digital from "../src/images/digital.jpg";
+import marketing from "../src/images/marketing.jpg";
+import software from "../src/images/software.jpg";
 
+const Sdata=[
+  {
+  imgsrc: web,
+    title "web developer",
+    
+  },
+  {
+  imgsrc: app,
+    title "app developer",
+    
+  },
+  {
+  imgsrc: software,
+    title "software developer",
+    
+  },
+  {
+  imgsrc: digital,
+    title "digital marketing",
+    
+  },
+  {
+  imgsrc: android,
+    title "Android developer",
+    
+  },
+   {
+  imgsrc: msrketing,
+    title "Marketing",
+    
+  },
+  ];
+export default Sdata;
