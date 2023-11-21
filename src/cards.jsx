ead3074871161b68b43322fdcc6f054916ba1671
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-21 09:50:40 +0500

    Update cards.jsx

--- src/cards.jsx
+++ src/cards.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import web from "../src/images/img1.jpg";
 import { NavLink} from "react-router-dom";
-const Card =()=>{
+const Card =(props)=>{
  return (
 
   <>
 
               <div className="col-md-4 col-10 mx-auto">
                       <div className="card" ">
-                          <img src={web} className="card-img-top" alt=""/>
+                          <img src={props.web} className="card-img-top" alt={props.imgsrc}/>
                           <div className="card-body">
-                              <h5 className="card-title">
-                               Card title
+                              <h5 className="card-title font-weight-bold">
+                               {props.title}
                               </h5>
                               <p className="card-text">
-                               Some example
+                              Some example
                               </p>
                               <NaveLink to="" className=""btn btn-primary"> Go somewhere </NaveLink>
                           </div>
