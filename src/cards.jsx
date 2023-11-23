79c077b08ea8a3a1b340c021bcc6a682a21a2d04
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-23 09:44:09 +0500

    Update cards.jsx

--- src/cards.jsx
+++ src/cards.jsx
@@ -1,34 +1,24 @@
 import React from "react";
 import web from "../src/images/img1.jpg";
-import { NavLink} from "react-router-dom";
-const Card =(props)=>{
- return (
+import { NavLink } from "react-router-dom";
 
-  <>
+const Cards = (props) => {
+  return (
+    <>
+      <div className="col-md-4 col-10 mx-auto">
+        <div className="card">
+          <img src={props.web} className="card-img-top" alt={props.imgsrc} />
+          <div className="card-body">
+            <h5 className="card-title font-weight-bold">{props.title}</h5>
+            <p className="card-text">Some example</p>
+            <NavLink to="" className="btn btn-primary">
+              Go somewhere
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-              <div className="col-md-4 col-10 mx-auto">
-                      <div className="card" ">
-                          <img src={props.web} className="card-img-top" alt={props.imgsrc}/>
-                          <div className="card-body">
-                              <h5 className="card-title font-weight-bold">
-                               {props.title}
-                              </h5>
-                              <p className="card-text">
-                              Some example
-                              </p>
-                              <NaveLink to="" className=""btn btn-primary"> Go somewhere </NaveLink>
-                          </div>
-                              
-                      </div>
-                   </div>
-   
- 
-  
-  </>
-
-   
- )
-
-
-  
-}
+export default Cards;
